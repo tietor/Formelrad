1207ed638d35a6801bb94d45fca771f990c67307
5ia17fatorti <5ia17fatorti@gibwintra.ch>
2018-11-05 08:17:31 +0100

    calculate P from I and R

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -2,56 +2,61 @@ package application;
 
 /**
  * Berechnet das Formelrad
+ *
  * @author Peter Rutschmann
  * @version 13.09.2018
  */
 public class Calculator {
-	private double leistung;
-	private double spannung;
-	private double strom;
-	private double widerstand;
+    private double leistung;
+    private double spannung;
+    private double strom;
+    private double widerstand;
 
-	public Calculator(double leistung, double spannung, double strom, double widerstand) {
-		super();
-		this.leistung = leistung;
-		this.spannung = spannung;
-		this.strom = strom;
-		this.widerstand = widerstand;
-	}
+    public Calculator(double leistung, double spannung, double strom, double widerstand) {
+        super();
+        this.leistung = leistung;
+        this.spannung = spannung;
+        this.strom = strom;
+        this.widerstand = widerstand;
+    }
 
-	public double getLeistung() {
-		return leistung;
-	}
+    public double getLeistung() {
+        return leistung;
+    }
 
-	public double getSpannung() {
-		return spannung;
-	}
+    public double getSpannung() {
+        return spannung;
+    }
 
-	public double getStrom() {
-		return strom;
-	}
+    public double getStrom() {
+        return strom;
+    }
 
-	public double getWiderstand() {
-		return widerstand;
-	}
+    public double getWiderstand() {
+        return widerstand;
+    }
 
-	@Override
-	public String toString() {
-		return "Calculator [leistung=" + leistung +
-				", spannung=" + spannung +
-				", strom=" + strom +
-				", widerstand="	+ widerstand + "]";
-	}
+    @Override
+    public String toString() {
+        return "Calculator [leistung=" + leistung +
+                ", spannung=" + spannung +
+                ", strom=" + strom +
+                ", widerstand=" + widerstand + "]";
+    }
 
-	public void calculate() {
-		/* Hier auf Grund der vorhanden Werte entscheiden
-		 * welche Methode unten aufgerufen werden muss.
-		 */
-	}
+    public void calculate() {
+        /* Hier auf Grund der vorhanden Werte entscheiden
+         * welche Methode unten aufgerufen werden muss.
+         */
+    }
 
-	/* Hier die Methoden mit den Formlen hinzuf�gen
-	 */
+    /* Hier die Methoden mit den Formlen hinzuf�gen
+     */
     public double pFromUandI(double u, double i) {
-        return u*i;
+        return u * i;
+    }
+
+    public double pFromIandR(double i, double r) {
+        return r * i * i;
     }
 }
